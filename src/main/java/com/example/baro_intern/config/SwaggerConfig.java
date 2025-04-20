6bf7351f1aa35b6e17844542b9b4c22d9e296673
duol9 <lhyy0195@gmail.com>
2025-04-21 05:40:05 +0900

    feat: Swagger Config 추가

---
+++ src/main/java/com/example/baro_intern/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.example.baro_intern.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.annotations.info.Info;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Spring API Document",
+        description = "Spring Boot 기반 JWT 인증/인가 및 AWS 배포",
+        version = "v0.0.1"
+    )
+)
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = getSecurityScheme();
+        SecurityRequirement securityRequirement = getSecurityRequireMent();
+
+        return new OpenAPI()
+            .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+            .security(List.of(securityRequirement));
+    }
+
+    private SecurityScheme getSecurityScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER).name("Authorization");
+    }
+
+    private SecurityRequirement getSecurityRequireMent() {
+        return new SecurityRequirement().addList("bearerAuth");
+    }
+}
